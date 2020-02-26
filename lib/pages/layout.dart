504840dabed6b5fcb94b4a4f1c8914c73b3df178
Kenji Sonohara <k_sonohara@ewings.biz>
2020-02-26 11:49:06 +0900

    ListWheelScrollViewを実装した。レイアウト用のページを作成した。

---
+++ lib/pages/layout.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LayoutPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('It is layout page.'),
+    );
+  }
+}
