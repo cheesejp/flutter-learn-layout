4ed003bda5d27c4b1351c12ea0290a53dcdb4508
Kenji Sonohara <k_sonohara@ewings.biz>
2020-02-27 09:45:16 +0900

    レイアウトのチュートリアルを学ぶためにListWheelScrollViewを別のプロジェクトに移した。

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:layout/pages/layout.dart';
-import 'package:layout/pages/list-wheel-scroll-view-page.dart';
 
 void main() => runApp(MyTab());
 
 class MyTab extends StatelessWidget {
-  final List<Widget> pages = [
-    LayoutPage(),
-    ListWheelScrollViewPage(title: 'ListWheelScrollView Demo')
-  ];
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        home: LayoutPage(
+          title: 'aaa',
+        ),
+      );
+}
+
+class LayoutPage extends StatelessWidget {
+  LayoutPage({@required this.title});
+  final String title;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
-          length: pages.length, child: TabBarView(children: pages)),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter layout demo'),
+      ),
+      body: Text('It is layout page.'),
     );
   }
 }
